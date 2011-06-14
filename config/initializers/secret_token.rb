c2f25e75f4fef7b5330ee89a04808716b704e9a4
Michael Huynh <mike.huynh@gmail.com>
2011-06-14 16:58:49 -0400

    Initial rails project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Albumfs::Application.config.secret_token = '904b00513c73fd39ce5b42a48c86c4bdc526348fa372f2871278f2b37f17ae9b7b88b0edb6fe41099c1a38a690578b555f993e9393ed767b9f6e43a4a78d567a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Albumfs::Application.config.session_store :cookie_store, :key => '_albumfs_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Albumfs::Application.config.session_store :active_record_store
