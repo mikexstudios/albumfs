c87667b33daaf029bd8188004f0d9416f718fe9c
Michael Huynh <mike.huynh@gmail.com>
2011-06-19 18:44:19 -0400

    Added title helper and set the title for home.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def title(page_title, include_base_title = true)
+    page_title += ' | AlbumFS' if include_base_title
+    content_for(:title, page_title.to_s)
+  end
 end
