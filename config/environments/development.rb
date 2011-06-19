ca584098d0a1a3b185e4f7b4167d964994873679
Michael Huynh <mike.huynh@gmail.com>
2011-06-19 19:22:45 -0400

    Fixed typo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Albumfr::Application.configure do
+Albumfs::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
