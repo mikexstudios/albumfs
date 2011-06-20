0ddcff9880d88c02fd8ab1ebf529c605893aba3c
Michael Huynh <mike.huynh@gmail.com>
2011-06-19 23:50:47 -0400

    Added unique id to each page's body.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,11 @@ module ApplicationHelper
     page_title += ' | AlbumFS' if include_base_title
     content_for(:title, page_title.to_s)
   end
+
+  # Returns a string of "action_controller" that is useful for identifying the
+  # page in CSS id/class names.
+  def location_name
+    "#{action_name}_#{controller.controller_name}"
+  end
+  
 end
