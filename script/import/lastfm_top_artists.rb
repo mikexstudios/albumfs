82672eaed5e05f55f161bb40b6878a266062d4de
Michael Huynh <mike.huynh@gmail.com>
2011-06-26 16:07:51 -0400

    Added standalone script to get top 1000 artists from lastfm.

---
+++ script/import/lastfm_top_artists.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+require File.expand_path('../../../config/environment.rb', __FILE__)
+
+require 'open-uri'
+require 'nokogiri'
+#require 'ruby-debug'
+
+API_CALL = 'http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=%s&limit=1000'
+
+doc = Nokogiri::XML(open(API_CALL % Rails.configuration.lastfm.apikey))
+
+doc.xpath('//name').each do |name|
+  #Print out name
+  puts name.text
+  
+  #Also populate database with it. If the Artist already exists, then don't create
+  #it again.
+  Artist.find_or_create_by_name(:name => name.text)
+end
