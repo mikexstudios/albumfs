730c664c7d7385b02826119ea662dcb4bc537f79
Michael Huynh <mike.huynh@gmail.com>
2011-07-03 00:01:25 -0400

    Setting root url to search page (which is essentially the home page except with a search box).

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,8 @@ Albumfs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'high_voltage/pages#show', :id => 'home'
+  #root :to => 'high_voltage/pages#show', :id => 'home'
+  root :to => 'search#index'
 
   # See how all your routes lay out with "rake routes"
 
