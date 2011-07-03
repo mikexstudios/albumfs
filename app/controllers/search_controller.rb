76657e4983a734ab49177068e75ef9f3e9508ba8
Michael Huynh <mike.huynh@gmail.com>
2011-07-03 02:01:32 -0400

    Added specifying page to the search results.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     if params[:q] and not params[:q].empty?
 
-      @results = Album.search_tank(params[:q])
+      @results = Album.search_tank(params[:q], :page => params[:page])
     else
       redirect_to root_path
     end
