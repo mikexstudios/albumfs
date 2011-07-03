0856f9c879cbf58d6a5ba3f3c116fc370d2355b1
Michael Huynh <mike.huynh@gmail.com>
2011-07-03 00:29:47 -0400

    Started to integrate querying.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,8 @@ class SearchController < ApplicationController
   def index
     if params[:q]
       redirect_to root_path if params[:q].empty?
+
+      @results = Album.search_tank(params[:q])
     end
   end
 
